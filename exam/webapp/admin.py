97e991b883daa86952ced86a302bb804793d5c94
Bars <barsbekclash@gmail.com>
2021-05-01 12:24:09 +0600

    add 2 models and migrate and register in admin

--- exam/webapp/admin.py
+++ exam/webapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from webapp.models import Product, Feedback
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'description', 'img']
+    list_filter = ['name']
+    search_fields = ['name', 'category', 'description']
+    fields = ['id', 'name', 'category', 'description', 'img']
+    readonly_fields = ['id']
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'product', 'feedback_text', 'created_at', 'updated_at', 'moder_check']
+    list_filter = ['product', 'author', 'moder_check']
+    search_fields = ['feedback_text',]
+    fields = ['id', 'author', 'product', 'feedback_text', 'appraisal', 'moder_check', 'created_at', 'updated_at']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
--- exam/webapp/models.py
+++ exam/webapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
+category_choice = (('electronic', 'Electronic'), ('house', 'House'), ('jobe', 'Jobe'), ('food', 'Food'), ('games', 'Games'), ('sundry', 'Sundry'))
 # Create your models here.
+
+class Product(models.Model):
+   name = models.CharField(max_length=200, null=False, blank=False)
+   category = models.CharField(max_length=200, null=False, blank=False, choices=category_choice, default='sundry')
+   description = models.TextField(max_length=2000, null=True, blank=True)
+   img = models.ImageField(null=True, blank=True, upload_to='product_pics', verbose_name='Картинка')
+
+   def __str__(self):
+       return self.name + "'s Product"
+
+   class Meta:
+       db_table = 'Product'
+       verbose_name = 'Товар'
+       verbose_name_plural = 'Товары'
+
+class Feedback(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='author',  verbose_name='Автор')
+    product = models.ForeignKey('webapp.Product', on_delete=models.CASCADE, related_name='product', verbose_name='Товар')
+    feedback_text = models.TextField(max_length=3000, blank=False, null=False)
+    appraisal = models.IntegerField(blank=False, null=False, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    moder_check = models.BooleanField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
