29b8d1b80937a5fc3c0684e092a59c259155586f
Bars <barsbekclash@gmail.com>
2021-05-24 16:41:58 +0600

    fixed error of output average appraisal

--- exam/webapp/views/product.py
+++ exam/webapp/views/product.py
@@ -76,7 +76,17 @@ class ProductView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        objects = self.get_object().product.all()
+        total = 0
+        qty = objects.count()
+        for i in objects:
+            total += i.appraisal
+        if total > 0:
+            average = total / qty
+        else:
+            average = "Not appraisal"
         context['feedbacks'] = self.get_object().product.filter(moder_check=True)
+        context['average'] = average
         return context
 
     # def dispatch(self, request, *args, **kwargs):
